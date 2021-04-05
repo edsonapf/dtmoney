c44f352a7ff9df8e2d61a44d61528016f50295bf
Edson Alves <edsonapf@gmail.com>
2021-04-04 22:42:04 -0300

    feat: add styling to modal component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -20,13 +23,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal 
-          isOpen={isNewTransactionModalOpen} 
-          onRequestClose={handleCloseNewTransactionModal}>
-          <h2>Cadastrar transação</h2>
-        </Modal>
-
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
       <GlobalStyle />
     </>
   );
